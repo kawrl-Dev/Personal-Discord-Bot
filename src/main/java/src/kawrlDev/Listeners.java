7e05c0bd6c0432bbb9971a25c7abe0f1e99099b2
Kawrl <karlalfonsogoliva@gmail.com>
2026-04-18 17:01:12 +0800

    Initial commit!

---
+++ src/main/java/src/kawrlDev/Listeners.java
@@ -0,0 +1,4 @@
+package src.kawrlDev;
+
+public class Listeners {
+}
---
+++ src/main/java/src/kawrlDev/MyDiscordBot.java
@@ -0,0 +1,7 @@
+package src.kawrlDev;
+
+public class Main {
+    public static void main() {
+        System.out.println("Hello, World!");
+    }
+}
