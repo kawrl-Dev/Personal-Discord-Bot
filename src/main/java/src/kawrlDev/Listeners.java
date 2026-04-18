def94c9118b0d7af611ab0c9006a7b13bb4da462
Kawrl <karlalfonsogoliva@gmail.com>
2026-04-18 17:11:45 +0800

    [4.18.2026] - Updated build.gradle, Listeners.java, MyDiscordBot.java, and added logback.xml

--- src/main/java/src/kawrlDev/Listeners.java
+++ src/main/java/src/kawrlDev/Listeners.java
@@ -1,7 +1,39 @@
 package src.kawrlDev;
 
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Listeners extends ListenerAdapter {
-    // placeholder comment lol
+    private static final Logger log = LoggerFactory.getLogger(Listeners.class);
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        Message message = event.getMessage();
+        String content = message.getContentRaw();
+
+        if (content.equals("!ping")){
+            MessageChannel channel = event.getGuildChannel();
+            channel.sendMessage("Pong!").queue();
+        }
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getName().equals("shutdown")) {
+
+            if (!event.getUser().getId().equals("418022453030289420")) {
+                event.reply("You don't have permission to do that.").setEphemeral(true).queue();
+                return;
+            }
+
+            event.reply("Shutting down... goodbye!").queue(_ -> {
+                log.info("Shutdown command issued by {}", event.getUser().getName());
+                event.getJDA().shutdown();
+            });
+        }
+    }
 }
--- src/main/java/src/kawrlDev/MyDiscordBot.java
+++ src/main/java/src/kawrlDev/MyDiscordBot.java
@@ -4,18 +4,29 @@ import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MyDiscordBot {
     private static final Dotenv dotenv = Dotenv.load();
-    public static JDA jda;
+    private static final Logger log = LoggerFactory.getLogger(MyDiscordBot.class);
     private static final String discordBotToken = dotenv.get("BOT_API");
     public static void main() {
         try {
-            jda = JDABuilder.createDefault(discordBotToken)
+            JDA jda = JDABuilder.createDefault(discordBotToken)
                     .setActivity(Activity.customStatus("Running in the background!"))
-                    .build();
+                    .addEventListeners(new Listeners())
+                    .build()
+                    .awaitReady();
+
+            jda.updateCommands().addCommands(
+                    Commands.slash("shutdown","Shuts down the Discord Bot.")
+            ).queue();
+
+            log.info("Bot Started Successfully");
         } catch (Exception e) {
-            System.out.println("Something went wrong: " + e);
+            log.error("Something went wrong: {}", e.toString());
         }
     }
 }
