c200a3112d4f2c98dae84cf500bfe008bb407990
Kawrl <karlalfonsogoliva@gmail.com>
2026-04-23 06:48:45 +0800

    [4.23.2026] - Added hashmap validation and menuBuilder for selecting tasks from the selected task list

--- src/main/java/dev/kawrl/botcommands/productivityfeatures/markTaskAsFinishedCommand/MarkSelectedTaskFactory.kt
+++ src/main/java/dev/kawrl/botcommands/productivityfeatures/markTaskAsFinishedCommand/MarkSelectedTaskFactory.kt
@@ -1,5 +1,6 @@
 package dev.kawrl.botcommands.productivityfeatures.markTaskAsFinishedCommand
 
+import dev.kawrl.database.TaskRepo
 import dev.kawrl.interfaces.CommandHandler
 import dev.kawrl.interfaces.CommandHandler.StringSelectMenuInterface
 import net.dv8tion.jda.api.components.actionrow.ActionRow
@@ -9,13 +10,23 @@ import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionE
 class MarkSelectedTaskFactory : CommandHandler(), StringSelectMenuInterface {
     override fun handle(event: StringSelectInteractionEvent) {
         val listId: String = event.values[0]
+        val taskList = TaskRepo.getTasksFromTaskListForUser(listId,event.user.id)
 
-        val taskSelectMenu: StringSelectMenu.Builder = StringSelectMenu.create("select-task")
-            .setPlaceholder("Choose One or Multiple Tasks")
+        if (!taskList.isEmpty()){
+            val taskSelectMenu: StringSelectMenu.Builder = StringSelectMenu.create("select-task")
+                .setPlaceholder("Choose One or Multiple Tasks")
+                .setRequiredRange(1,(taskList.size))
 
-        event.reply("Select the task items that will be marked as finished").addComponents(
-            ActionRow.of(taskSelectMenu.build()))
-            .setEphemeral(true)
-            .queue()
+            taskList.forEach { task, id -> (taskSelectMenu.addOption(task,id.toString())) }
+            event.reply("Select the task items that will be marked as finished").addComponents(
+                ActionRow.of(taskSelectMenu.build()))
+                .setEphemeral(true)
+                .queue()
+        }
+        else{
+            event.reply("You have no tasks in this list yet! Create one first with '/add-task'")
+                .setEphemeral(true)
+                .queue()
+        }
     }
 }
