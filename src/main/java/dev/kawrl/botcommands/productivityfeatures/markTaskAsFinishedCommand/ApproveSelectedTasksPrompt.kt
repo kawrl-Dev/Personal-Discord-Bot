587ae21ef5b788eaf35f33176bcd4e61a1244592
Kawrl <karlalfonsogoliva@gmail.com>
2026-04-23 14:52:03 +0800

    [4.23.2026] - Added the listID component to the method

--- src/main/java/dev/kawrl/botcommands/productivityfeatures/markTaskAsFinishedCommand/ApproveSelectedTasksPrompt.kt
+++ src/main/java/dev/kawrl/botcommands/productivityfeatures/markTaskAsFinishedCommand/ApproveSelectedTasksPrompt.kt
@@ -5,15 +5,16 @@ import net.dv8tion.jda.api.components.actionrow.ActionRow
 import net.dv8tion.jda.api.components.buttons.Button
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent
 
-class ApproveSelectedTasksPrompt: CommandHandler(), CommandHandler.StringSelectMenuInterface {
+class ApproveSelectedTasksPrompt : CommandHandler(), CommandHandler.StringSelectMenuInterface {
     override fun handle(event: StringSelectInteractionEvent) {
+        val listID = event.componentId.removePrefix("approve-selected-tasks:")
         val encodedIDs: String = event.values.joinToString(",")
 
-        val yesButton: Button = Button.success("yes-response:$encodedIDs","All good!")
-        val noButton: Button = Button.danger("no-response","Actually, nevermind!")
+        val yesButton: Button = Button.success("yes-response:$listID|$encodedIDs", "All good!")
+        val noButton: Button = Button.danger("no-response", "Actually, nevermind!")
 
         event.reply("Do you want to mark the selected tasks as Finished?")
-            .addComponents(ActionRow.of(yesButton,noButton))
+            .addComponents(ActionRow.of(yesButton, noButton))
             .setEphemeral(true)
             .queue()
     }
