a69c83c594cc05c6b13efb6f34ef3384111296e1
Kawrl <karlalfonsogoliva@gmail.com>
2026-04-23 14:50:05 +0800

    [4.23.2026] - Updated the interaction for CancelMarkTasks.java

--- src/main/java/dev/kawrl/botcommands/productivityfeatures/markTaskAsFinishedCommand/CancelMarkTasks.java
+++ src/main/java/dev/kawrl/botcommands/productivityfeatures/markTaskAsFinishedCommand/CancelMarkTasks.java
@@ -1,14 +1,12 @@
 package dev.kawrl.botcommands.productivityfeatures.markTaskAsFinishedCommand;
 
 import dev.kawrl.interfaces.CommandHandler;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 
 public class CancelMarkTasks extends CommandHandler implements CommandHandler.ButtonInterface {
     @Override
     public void handle(ButtonInteractionEvent event) {
-        Message message = event.getMessage();
-        message.delete().queue();
-        logger.info("Process of marking tasks as done cancelled by '{}'",event.getUser());
+        logger.info("Process of marking tasks as done cancelled by '{}'", event.getUser().getName());
+        event.reply("Cancelled.").setEphemeral(true).queue();
     }
 }
