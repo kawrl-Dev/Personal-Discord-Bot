a9d68f9576e896dba2cd5db10cd4ffdf188a9428
Kawrl <karlalfonsogoliva@gmail.com>
2026-04-23 11:02:44 +0800

    [4.23.2026] - Fixed menuID in MarkSelectedTaskFactory.kt

--- src/main/java/dev/kawrl/botcommands/productivityfeatures/markTaskAsFinishedCommand/MarkSelectedTaskFactory.kt
+++ src/main/java/dev/kawrl/botcommands/productivityfeatures/markTaskAsFinishedCommand/MarkSelectedTaskFactory.kt
@@ -13,7 +13,7 @@ class MarkSelectedTaskFactory : CommandHandler(), StringSelectMenuInterface {
         val taskList = TaskRepo.getTasksFromTaskListForUser(listId,event.user.id)
 
         if (!taskList.isEmpty()){
-            val taskSelectMenu: StringSelectMenu.Builder = StringSelectMenu.create("select-task:$listId")
+            val taskSelectMenu: StringSelectMenu.Builder = StringSelectMenu.create("approve-selected-tasks")
                 .setPlaceholder("Choose One or Multiple Tasks")
                 .setRequiredRange(1,(taskList.size))
 
