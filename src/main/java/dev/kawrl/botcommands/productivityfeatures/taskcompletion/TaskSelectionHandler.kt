d323ed1b9e0515b3f902775de3f2f96642bb085e
Kawrl <karlalfonsogoliva@gmail.com>
2026-04-23 20:03:01 +0800

    [4.23.2026] - Added non-null asserted call to username variable

--- src/main/java/dev/kawrl/botcommands/productivityfeatures/taskcompletion/TaskSelectionHandler.kt
+++ src/main/java/dev/kawrl/botcommands/productivityfeatures/taskcompletion/TaskSelectionHandler.kt
@@ -6,25 +6,35 @@ import dev.kawrl.interfaces.CommandHandler.StringSelectMenuInterface
 import net.dv8tion.jda.api.components.actionrow.ActionRow
 import net.dv8tion.jda.api.components.selections.StringSelectMenu
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent
+import java.sql.SQLException
 
 class TaskSelectionHandler : CommandHandler(), StringSelectMenuInterface {
     override fun handle(event: StringSelectInteractionEvent) {
+        val member = event.member
+        val username = member!!.user.name
         val listId: String = event.values[0]
-        val taskList = TaskRepo.getTasksFromTaskListForUser(listId,event.user.id)
+        try {
+            val taskList = TaskRepo.getTasksFromTaskListForUser(listId,event.user.id)
 
-        if (!taskList.isEmpty()){
-            val taskSelectMenu: StringSelectMenu.Builder = StringSelectMenu.create("approve-selected-tasks:$listId")
-                .setPlaceholder("Choose One or Multiple Tasks")
-                .setRequiredRange(1,(taskList.size))
+            if (!taskList.isEmpty()){
+                val taskSelectMenu: StringSelectMenu.Builder = StringSelectMenu.create("approve-selected-tasks:$listId")
+                    .setPlaceholder("Choose One or Multiple Tasks")
+                    .setRequiredRange(1,(taskList.size))
 
-            taskList.forEach { task, id -> (taskSelectMenu.addOption(task,id.toString())) }
-            event.reply("Select the task items that will be marked as finished").addComponents(
-                ActionRow.of(taskSelectMenu.build()))
-                .setEphemeral(true)
-                .queue()
-        }
-        else{
-            event.reply("You have no tasks in this list yet! Create one first with '/add-task'")
+                taskList.forEach { task, id -> (taskSelectMenu.addOption(task,id.toString())) }
+                event.reply("Select the task items that will be marked as finished").addComponents(
+                    ActionRow.of(taskSelectMenu.build()))
+                    .setEphemeral(true)
+                    .queue()
+            }
+            else{
+                event.reply("You have no tasks in this list yet! Create one first with '/add-task'")
+                    .setEphemeral(true)
+                    .queue()
+            }
+        } catch (e: SQLException){
+            logger.warn("Database error while getting task lists from user '{}': {}",username, e.toString())
+            event.reply("A database error occurred. Please try again later.")
                 .setEphemeral(true)
                 .queue()
         }
